000e8d0ee9bd2b084579394664eeb9ebab8fb1ea
AvinavStha <avi.shrestha.775@gmail.com>
2022-04-07 13:42:27 +0545

    second commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,124 +1,101 @@
 import React from "react";
 import "./home.css";
-import homepage from '../../assets/images/homepage.png';
-import arrow from '../../assets/images/arrow.png';
-import proactiveSolutions from '../../assets/images/proactiveSolutions.png';
-import simplifyWork from '../../assets/images/simplifyWork.png';
-import saveTime from '../../assets/images/saveTime.png';
+import homepage from "../../assets/images/homepage.png";
+import arrow from "../../assets/images/arrow.png";
+import proactiveSolutions from "../../assets/images/proactiveSolutions.png";
+import simplifyWork from "../../assets/images/simplifyWork.png";
+import saveTime from "../../assets/images/saveTime.png";
 
 const Home = () => {
   return (
     <>
-      <div className=" heading p-5 text-center bg-light">
-        <header className="masthead d-flex">
-          <div className="container-fluid h-100 my-auto">
-            <div className="row h-100 align-items-center justify-content-center">
-              <div className="col">
-                <div
-                  className="
-              d-flex
-              flex-column
-              align-items-center
-              px-3 px-md-5
-              pt-5 pt-lg-0"
-                >
-                  <div className="hello px-2 px-md-2 pt-2 pt-lg-0">
-                    <p className="text-header text-header-md text-center font-weight-bold">
-                      Deliver Exceptional Customer Service <br />
-                      with Anydone Desk
-                    </p>
-
-                    <h4 className="text-header-des semi color-description text-center mb-4">
-                      Offer a delightful conversational support that is
-                      immediate and friendly. Exceed your <br />
-                      customer’s expectation with Anydone Desk's AI powered
-                      support system.
-                    </h4>
-                    <div className="pricing-btn-group">
-                      <a
-                        className=" button
-                btn btn-primary cta-button-primary_rounded
-                  cta-button-primary_lg cta-button-primary_rounded_lg
-                  text-uppercase
-                  mb-0 mb-sm-0
-                  mt-2 pl-20
-                "
-                        href="#"
-                      >
-                        Try Now
-                      </a>
-                      <a
-                        className=" button1 btn cta-button-outline	cta-button-primary_rounded f-sm mb-5 mb-lg-0"
-                        href="#"
-                        onclick="openVideo()"
-                      >
-                        Watch Video
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div className="container">
+        <header className="main-heading">
+          <div className="landing">
+            <p className="text-header">
+              Deliver Exceptional Customer Service <br />
+              with Anydone Desk
+            </p>
+            <h4 className="subtext-header">
+              Offer a delightful conversational support that is immediate and
+              friendly. Exceed your <br />
+              customer’s expectation with Anydone Desk's AI powered support
+              system.
+            </h4>
+            <div className="heading-button">
+              <a className="trynow-btn" href="#">
+                Try Now
+              </a>
+              <a className="video-btn" href="#" onclick="openVideo()">
+                Watch Video
+              </a>
             </div>
           </div>
         </header>
       </div>
-      <div className="container-fluid h-150">
-        <div className="homepage">
+      <section className="homepage">
+        <div className="homepage-img">
           <img src={homepage} alt="homepage" />
         </div>
-        <div className="row h-100 align-items-center justify-content-center align-items-lg-start">
-          <div className="col-md-12 col-lg-7 order-1 ">
-            <div className="d-flex flex-column align-items-lg-start px-2 px-md-4  ">
-              <p className="text-title color-header mb-1 mb-md-5 ">
-                <b>Proactive Solutions</b>{" "}
-              </p>
-            </div>
-            <div
-              className="proactive-quote-label d-flex flex-column  align-items-center align-items-lg-start
-            		px-9 px-md-6"
-            >
-              <p className="text-quote mb-4  text-lg-left">
-                <b>
-                  "Wouldn't it be{" "}
-                  <span className="color">
-                    <b>AWESOME</b>
-                  </span>{" "}
-                  if your problems were
-                  <br /> Solved before you even realize they existed.
-                </b>
-              </p>
-              <p
-                className=" paragraph
-									mb-4
-									text-desc-sm
-									font-weight
-									color-description
-									text-center text-lg-left
-									home-subtext
-								"
-              >
-                Interactively address known customer support issues, resolve,
-                and update flutter even before your customers reach you.
-              </p>
-              <a
-                className=" arrow btn cta-button-outline	cta-button-primary_rounded f-sm mb-5 mb-lg-0"
-                href="features.html"
-              >
-                Learn more
-                <span>
-                  <img src={arrow} alt="arrow" />
-                </span>
-              </a>
-            </div>
+        <div className="homepage-content">
+          <div className="proactive">
+            <p className="content-title">Proactive Solutions</p>
+            <p className="proactive-text">
+              Wouldn't it be
+              <span className="color"> AWESOME </span>
+              if your problems were Solved before you even realize they existed.
+            </p>
+            <p className="paragraph">
+              Interactively address known customer support issues, resolve, and
+              update flutter even before your customers reach you.
+            </p>
+            <a className="arrow">
+              Learn more
+              <span>
+                <img src={arrow} alt="arrow" />
+              </span>
+            </a>
           </div>
-          <div className="col-md-12 col-lg-4 order-2 text-center">
+          <div className="proactive-image">
             <img
-              className="proactive"
+              className="proactive-img"
               src={proactiveSolutions}
               alt="proactiveSolutions"
             />
           </div>
         </div>
+      </section>
+      <section className="homepage">
+        <div className="homepage-content">
+          <div className="proactive-image">
+            <img
+              className="simplify-img"
+              src={simplifyWork}
+              alt="simplifyWork"
+            />
+          </div>
+          <div className="proactive">
+            <p className="content-title">Proactive Solutions</p>
+            <p className="proactive-text">
+              Wouldn't it be
+              <span className="color"> AWESOME </span>
+              if your problems were Solved before you even realize they existed.
+            </p>
+            <p className="paragraph">
+              Interactively address known customer support issues, resolve, and
+              update flutter even before your customers reach you.
+            </p>
+            <a className="arrow">
+              Learn more
+              <span>
+                <img src={arrow} alt="arrow" />
+              </span>
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section>
         <div className="row h-100 align-items-center justify-content-center">
           <div className="col-md-12 col-lg-4 order-2 order-lg-1 text-center">
             <img className="simplify" src={simplifyWork} alt="simplifyWork" />
@@ -239,7 +216,7 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
